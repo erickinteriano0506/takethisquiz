5b7b02c245c5fe861b60b6b8e7352d666f3d199c
E Alexander <erick.interiano05@gmail.com>
2021-02-17 21:14:28 -0800

    Added variables for the questions and answers

---
+++ Assets/questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+        title: "Which of these is not an infiniy stone?",
+        choices: ["Time Stone", "Power Stone", "Mind Stone", "Hyper Stone"],
+        answer: "Hyper Stone"
+    },
+
+    {
+        title: "Who is the First Avenger?",
+        choices: ["Loki", "Nick Fury", "Captain America", "Batman"],
+        answer: "Captain America"
+    },
+    {
+        title: "Which of the following is not a member of the Avengers?",
+        choices: ["Thor", "Hawkeye", "The Punisher", "Black Widow"],
+        answer: "The Punisher"
+    },
+    {
+        title: "What planet is the Soul Stone found in?",
+        choices: ["Mars", "Vormir", "Ego", "Metropolis"],
+        answer: "Vormir"
+    },
+];
